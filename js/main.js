5e726fc2d30f549d6f438fa0c9eff2b44c2d13be
puhrome <phirom.yim@fullsail.edu>
2015-12-06 16:18:19 -0500

    CSS styling for portfolio

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by puhrome on 12/6/15.
+ */
+$(document).ready(function() {
+    $('[data-toggle=offcanvas]').click(function() {
+        $('.row-offcanvas').toggleClass('active');
+    });
+});
